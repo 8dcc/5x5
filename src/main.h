45b6a12d4f6b0c42d72ae55105833194af6ea288
8dcc <8dcc.git@gmail.com>
2023-04-12 23:52:54 +0200

    Add header

---
+++ src/main.h
@@ -0,0 +1,54 @@
+#ifndef _MAIN_H
+#define _MAIN_H 1
+
+#include <stdint.h>
+
+#define DEFAULT_W 5 /**< @brief Default width */
+#define DEFAULT_H 5 /**< @brief Default height */
+#define DEFAULT_S 3 /**< @brief Default scale */
+#define MIN_W     5 /**< @brief Minimum width */
+#define MIN_H     5 /**< @brief Minimum height */
+
+/**
+ * @def KEY_CTRLC
+ * @brief Needed for getch()
+ */
+#define KEY_CTRLC 3
+
+/**
+ * @name Characters for the tiles
+ * @{ */
+#define ON_CH  '#' /**< @brief On tile */
+#define OFF_CH '.' /**< @brief Off tile */
+/** @} */
+
+/**
+ * @struct point_t
+ * @brief Point in the **context grid**.
+ * @details Not a point on the real terminal, but on the grid. This is useful
+ * for moving to the real terminal position after rendering the scalled grid.
+ */
+typedef struct {
+    uint16_t y; /**< @brief Y coordinate */
+    uint16_t x; /**< @brief X coordinate */
+} point_t;
+
+/**
+ * @struct ctx_t
+ * @brief Context used for the game.
+ */
+typedef struct {
+    uint16_t w;     /**< @brief Grid width */
+    uint16_t h;     /**< @brief Grid height */
+    uint16_t sc;    /**< @brief Grid scale for rendering */
+    point_t cursor; /**< @brief User position inside the grid */
+
+    /**
+     * @brief Pointer to the game grid.
+     * @details It will save 1 char per cell, scale will be used for rendering
+     * only.
+     * */
+    uint8_t* grid;
+} ctx_t;
+
+#endif /* MAIN_H_ */
